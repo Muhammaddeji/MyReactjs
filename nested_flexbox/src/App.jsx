8206cb1e824f5b78cbaba7a6c477c30aed50f012
Muhammaddeji <adegbitesheriff300@gmail.com>
2025-08-31 21:46:24 +0100

    nested_flexbox

--- nested_flexbox/src/App.jsx
+++ nested_flexbox/src/App.jsx
@@ -2,42 +2,83 @@ function App() {
 
 
   return (
+     
+    
 
-    <div className=" flex flex-col min-h-screen">
-
-      <header className="bg-green-200 text-center font-extrabold p-5"> HEADER </header>
-
-      <div className="flex flex-1">
-
-        <aside className="bg-amber-300 w-1/4 flex items-center justify-center font-bold"> 
-        
-        ASIDE 
-        
-        </aside>
-
-        <main className="flex flex-col flex-1">
-
-          <nav className="bg-blue-300 text-center font-bold p-4"> 
-            
-            NAV
-            
-            </nav>
-
-          <article className="bg-neutral-500 flex-1 flex items-center justify-center font-bold"> 
-            
-            ARTICLE 
-            
-          </article>
-
-        </main>
+    <div className='flex flex-col min-h-screen'>
+      
+      <div className='flex justify-between p-4 font-bold bg-pink-200'>
+        <p> LOGO </p>
+        <p> SIGN UP </p>
+        <p> LOGIN</p>
 
       </div>
 
-      <footer className="bg-red-300 text-center font-bold p-4">
+
+
+      <div className='flex flex-1'>
+
+        <div className='w-1/5 p-4 text-start gap-4 flex flex-col font-bold bg-purple-100 justify-start'>
+
+        <p> DASHBOARD </p>
+        <p> MY COURSES </p>
+        <p> ASSIGNMENTS </p>
+        <p> CALENDAR </p>
+        <p> DISCUSION </p>
+        <p> LIBRARY</p>
         
-       FOOTER 
-       
-      </footer>
+        </div>
+
+
+
+        <div className='flex flex-col flex-1'>
+
+          <div className='p-4 justify-evenly flex flex-row font-bold bg-blue-200'>
+            <p> HOME </p>
+            <p> INSTRUCTORS </p>
+            <p> CERTIFICATES </p>
+            <p> SUPPORT </p>
+
+          </div>
+
+
+          <div className='p-5 flex-1 font-bold pl-14 bg-green-100 text-start gap-4'>
+            <h2 className='font-extrabold'> Course Contents </h2>
+            <ul className='list-disc pl-7'>
+              <li> Introduction to Machine learning: key concepts and applications </li>
+              <li> The French Revolution explained for beginners </li>
+              <li> Mastering basic algebra: step-by-step guide </li>
+              <li> Fundermentals of web development: HTML, CSS and Javascript </li>
+              <li> Essential study skills: how to learn more effectively </li>
+            </ul>
+
+            
+            <h2 className='font-extrabold mt-5'> Discussion Thread </h2>
+            <ul className='list-disc pl-7'>
+              <li> How to ask good question in online Forum </li>
+              <li> The power of peer learning: why students discussion matter </li>
+              <li> Etiquette for online academic discussion </li>
+              <li> From question to insight: Turning curiosity into learning </li>
+              <li> How to contribute meaningfully to class Q and A sessions </li>
+
+            </ul>
+            
+            </div>
+
+
+            <div className='p-4 text-center font-bold bg-orange-100 justify-evenly flex flex-row'>
+              <p> About</p>
+              <p> Contacts </p>
+              <p> Terms of service </p>
+              <p> Copyright </p>
+
+            </div>
+
+
+        </div>
+
+
+      </div>
 
 
     </div>
